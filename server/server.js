9aaeadc8089f872a2330755230417f23f19816aa
kumari <kakikumari111@gmail.com>
2026-07-14 09:14:59 +0000

    rollback

--- server/server.js
+++ server/server.js
@@ -1,48 +1,49 @@
-const express = require("express");
-const cors = require("cors");
-const dotenv = require("dotenv");
-const path = require("path");
-
-dotenv.config();
-
-const connectDB = require("./config/db");
-
-// Routes
 const authRoutes = require("./routes/authRoutes");
 const jobRoutes = require("./routes/jobRoutes");
 const applicationRoutes = require("./routes/applicationRoutes");
 const adminRoutes = require("./routes/adminRoutes");
 
+require("dotenv").config();
+
+console.log(process.env.MONGODB_URI);
+
+const connectDB = require("./config/db");
+
+const express = require("express");
+const cors = require("cors");
+
 const app = express();
+
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
-// Serve uploaded files
+// Resume files access
 app.use("/uploads", express.static("uploads"));
 
-// API Routes
+
+// Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/jobs", jobRoutes);
 app.use("/api/applications", applicationRoutes);
 app.use("/api/admin", adminRoutes);
 
-// Serve React Frontend
-app.use(express.static(path.join(__dirname, "../client/dist")));
 
-// Handle React routes
-app.use((req, res) => {
-  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+// Home Route
+app.get("/", (req, res) => {
+  res.send("Placement Portal Backend Running...");
 });
 
-// Connect Database
+
+const PORT = 5000;
+
+
+// Database Connection
 connectDB();
 
-// Port
-const PORT = process.env.PORT || 5000;
 
-// Start Server
+// Server Start
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
